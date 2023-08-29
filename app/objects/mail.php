30a08c5c5a6736f32266b04a62315c80341d427a
Klyausov Andrey <48391443+ak-flash@users.noreply.github.com>
2023-08-29 16:05:54 +0300

    Update mail.php

--- app/objects/mail.php
+++ app/objects/mail.php
@@ -19,13 +19,13 @@ try {
     $mail->isSMTP();                                            // Send using SMTP
     $mail->Host       = 'smtp.yandex.ru';                    // Set the SMTP server to send through
     $mail->SMTPAuth   = true;                                   // Enable SMTP authentication
-    $mail->Username   = 'noreply@immunology.tk';                     // SMTP username
-    $mail->Password   = 'noreply15650';                               // SMTP password
+    $mail->Username   = 'noreply@';                     // SMTP username
+    $mail->Password   = '';                               // SMTP password
     $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;         // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
     $mail->Port       = 465;                                    // TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
 
     //Recipients
-    $mail->setFrom('noreply@immunology.tk', 'Immunology CRM Info');
+    $mail->setFrom('noreply@', 'Immunology CRM Info');
     $mail->addAddress($user_email, '');     // Add a recipient
     $mail->CharSet = 'UTF-8';
     // Content
